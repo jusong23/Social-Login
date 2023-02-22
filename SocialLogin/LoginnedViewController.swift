ca06583899114a53895fdc37245760d6e53dde20
jusong23 <jusong23@naver.com>
2023-02-22 10:10:50 +0900

    Pod init

---
+++ SocialLogin/LoginnedViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginnedViewController.swift
+//  SocialLogin
+//
+//  Created by mobile on 2023/02/22.
+//
+
+import UIKit
+
+class LoginnedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SocialLogin/ViewController.swift
+++ SocialLogin/ViewController.swift
@@ -14,6 +14,29 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tappedGoogleLogin(_ sender: Any) {
+        guard let loginnedViewController = self.storyboard?.instantiateViewController(withIdentifier: "LoginnedViewController") as? LoginnedViewController else { return }
+        
+        
+        
+        self.navigationController?.pushViewController(loginnedViewController, animated: true)
+    }
+    
+    @IBAction func tappedKakaoLogin(_ sender: Any) {
+        guard let loginnedViewController = self.storyboard?.instantiateViewController(withIdentifier: "LoginnedViewController") as? LoginnedViewController else { return }
+        
+        
+        
+        self.navigationController?.pushViewController(loginnedViewController, animated: true)
+    }
+    
+    @IBAction func tappedAppleLogin(_ sender: Any) {
+        guard let loginnedViewController = self.storyboard?.instantiateViewController(withIdentifier: "LoginnedViewController") as? LoginnedViewController else { return }
+        
+        
+        
+        self.navigationController?.pushViewController(loginnedViewController, animated: true)
+    }
+    
 }
 
